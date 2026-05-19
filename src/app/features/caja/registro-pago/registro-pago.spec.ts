5ec8114da51e2e62672066ea0629333e7c935c5c
JORED666 <243842@ids.upchiapas.edu.mx>
2026-05-18 18:05:50 -0600

    feat: initial commit

---
+++ src/app/features/caja/registro-pago/registro-pago.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistroPago } from './registro-pago';
+
+describe('RegistroPago', () => {
+  let component: RegistroPago;
+  let fixture: ComponentFixture<RegistroPago>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegistroPago],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RegistroPago);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/caja/registro-pago/registro-pago.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-pago',
+  imports: [],
+  templateUrl: './registro-pago.html',
+  styleUrl: './registro-pago.css',
+})
+export class RegistroPago {}
