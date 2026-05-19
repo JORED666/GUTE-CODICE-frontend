f3c18eb4a17469aef0cd537f17c5ae067c0a0a1e
JORED666 <243842@ids.upchiapas.edu.mx>
2026-05-19 06:33:22 -0600

    feat: add imports in module products

--- src/app/features/productos/productos-module.ts
+++ src/app/features/productos/productos-module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductosRoutingModule } from './productos-routing-module';
+import { ListaProductos } from './lista-productos/lista-productos';
+import { FormProducto } from './form-producto/form-producto';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ProductosRoutingModule],
+  imports: [CommonModule, ProductosRoutingModule, ListaProductos, FormProducto]
 })
-export class ProductosModule {}
+export class ProductosModule { }
