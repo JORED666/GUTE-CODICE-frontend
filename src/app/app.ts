f316a060d4a8af90c368b7d483bce53af78b03a1
JORED666 <243842@ids.upchiapas.edu.mx>
2026-05-19 08:17:07 -0600

    feat: layout with PrimeNG icons and improved sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'CODICE-FRONTEND';
   menuOpen = false;
+  sidebarExpanded = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
