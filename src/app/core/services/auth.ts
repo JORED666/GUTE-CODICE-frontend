645c010d86825b5798278a4199b5b1ab0810989b
JORED666 <243842@ids.upchiapas.edu.mx>
2026-05-22 22:57:13 -0600

    feat: authentication service

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {}
+export class AuthService {
+
+  private apiUrl = `${environment.apiUrl}/auth`;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(correo: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { correo, password }).pipe(
+      tap((res: any) => {
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('admin', JSON.stringify(res.admin));
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('admin');
+    this.router.navigate(['/login']);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getAdmin(): any {
+    const admin = localStorage.getItem('admin');
+    return admin ? JSON.parse(admin) : null;
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
