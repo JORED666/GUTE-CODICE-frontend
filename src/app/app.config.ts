e7c2aa9f7165e683a2df6b4e9171315ebba10805
JORED666 <243842@ids.upchiapas.edu.mx>
2026-05-23 19:20:19 -0600

    feat: client modules and products connected to the backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     providePrimeNG({
       theme: {
         preset: Aura,
--- src/app/core/services/productos.ts
+++ src/app/core/services/productos.ts
@@ -1,6 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { AuthService } from './auth';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Productos {}
+export class ProductosService {
+
+  private apiUrl = `${environment.apiUrl}/productos`;
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.authService.getToken()}`
+    });
+  }
+
+  getProductos(): Observable<any[]> {
+  return this.http.get<any[]>(`${this.apiUrl}?t=${Date.now()}`, { headers: this.getHeaders() });
+  }
+
+  createProducto(producto: any): Observable<any> {
+    return this.http.post(this.apiUrl, producto, { headers: this.getHeaders() });
+  }
+
+  updateProducto(id: number, producto: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, producto, { headers: this.getHeaders() });
+  }
+
+  deleteProducto(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
+}
